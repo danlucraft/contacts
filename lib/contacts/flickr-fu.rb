6d4ca22a5135d95e0bd81a9561d15120900844e5
aurelian <oancea@gmail.com>
2009-08-10 23:11:02 +0200

    Maybe we can use flick-fu gem for Flickr contacts

---
+++ lib/contacts/flickr-fu.rb
@@ -0,0 +1,20 @@
+require 'contacts'
+
+require 'flickr-fu'
+
+module Contacts
+
+  class FlickrFu
+
+    def initialize(config_file=CONFIG_FILE)
+    end    
+
+    def get_authentication_url(appdata= nil)
+    end
+
+    def contacts(token)
+    end
+
+  end
+
+end
