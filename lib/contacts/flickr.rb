0c11ddb26754b25e031309ce0a06821157bcc184
aurelian <oancea@gmail.com>
2009-08-11 09:43:54 +0200

    Should have Flickr contacts, with FlickrFu

--- lib/contacts/flickr.rb
+++ lib/contacts/flickr.rb
@@ -1,11 +1,13 @@
 require 'contacts'
 
-require 'rubygems'
 require 'flickr_fu'
 
 module Contacts
   
   class Flickr
+
+    CONFIG_FILE = File.dirname(__FILE__) + '/../config/contacts.yml'
+
     def initialize(config_file=CONFIG_FILE)
       confs = YAML.load_file(config_file)['flickr']
       @appid= confs['appid']
@@ -17,6 +19,7 @@ module Contacts
     end
 
     def contacts(token)
+      ::Flickr.new({:key => @appid, :secret => @secret, :token => token}).contacts
     end
   end 
 end
